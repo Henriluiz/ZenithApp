8b9e16ef8e78fb24dd9c0ad0e1cf5bd690803acf
Henriluiz <luizao.lids@gmail.com>
2026-02-14 18:56:52 -0300

    Estruturamento de validação v0.5

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -5,13 +5,23 @@ import styles from './styles';
 
 export default function Login() {
 
-    const [email, SetEmail] = useState("");
-    const [senha, setSenha] = useState("");
+    const [email, SetEmail] = useState(null);
+    const [senha, setSenha] = useState(null);
     const esqueceu_senha = (idModal) => {
         navigation.navigate(idModal)
     }
-    const enviar = (idModal ) => {
+    const entrar = (idModal ) => {
         navigation.navigate(idModal)
+    }
+
+    const validarCampos = (email, senha) => {
+        enviarFrom = true;
+        if (!email.value) { console.log('Nome está vazio'); // ! Evitar comandos DROP etc.. SQL
+        enviarForm = false; } else if (!senha.value) { console.log('Senha está vazia');
+        enviarForm = false; } else if (senha.value.length < 4 || senha.value.length > 10) { console.log('Senha deve ter entre 4 e 10 caracteres');
+        enviarForm = false; } 
+        if (enviarForm) { console.log('Formulário válido, enviando...');
+        }; // Aqui terá uma função responsável para ligar ou  
     }
 
     return (
@@ -38,8 +48,9 @@ export default function Login() {
                                 style={styles.input}
                                 onChangeText={SetEmail}
                                 value={email}
-                                keyboardType="email"
+                                keyboardType="email-address"
                                 underlineColorAndroid="transparent" // Remove aquela linha em baixo das palavras, enquanto escrevemos!
+                                maxLength={100}
                             />
                         </View>
                     </View>
@@ -49,10 +60,11 @@ export default function Login() {
                             <Image source={require('./img/cadeado.png')}/>
                             <TextInput
                                 style={styles.input}
-                                onChangeText={SetEmail}
-                                value={email}
-                                keyboardType="email"
+                                onChangeText={setSenha}
+                                value={senha}
+                                keyboardType="visible-password"
                                 underlineColorAndroid="transparent" // Remove aquela linha em baixo das palavras, enquanto escrevemos!
+                                maxLength={20}
                             />
                         </View>
                         <Pressable onPress={() => esqueceu_senha('login')}>
@@ -65,7 +77,7 @@ export default function Login() {
                 {/* Início do botão */}
                 <View style={styles.contEntra}>
                     <View style={styles.botaoEntra}>
-                        <Pressable onPress={() => enviar('login')} style={styles.stylesButton}>
+                        <Pressable onPress={() => validarCampos(email, senha)} style={styles.stylesButton}>
                             <Text style={styles.entrarText}>Entrar</Text>
                             <View>
                                 <ImageBackground style={styles.imgfSeta} source={require('./img/fundoIcone.png')}/>
